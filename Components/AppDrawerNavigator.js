7894ad94faae255d8303be7194eae5731c7b0622
Poonam-Lunawat <68240432+Poonam-Lunawat@users.noreply.github.com>
2021-01-17 22:42:54 +0530

    Add files via upload
    user profiles added

---
+++ Components/AppDrawerNavigator.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { AppTabNavigator } from "./AppTabNavigator";
+import CustomSideBarMenu from './CustomSideBarMenu';
+import  SettingScreen from '../Screens/SettingScreen'
+
+export const AppDrawerNavigator = createDrawerNavigator({
+   Home : {
+        screen:AppTabNavigator
+    },
+    Settings :{
+        screen : SettingScreen
+    },
+},
+    {
+        contentComponent : CustomSideBarMenu
+    },
+    {
+        initialRouteName: 'Home'
+    }
+)
